12edf10126b838269036f18f6faf2047a03cb7f1
Pierre Carion <pcarion@gmail.com>
2021-04-13 19:04:01 -0700

    restore resizing

--- src/actions/resizePodcastImage.ts
+++ src/actions/resizePodcastImage.ts
@@ -1,4 +1,4 @@
-//import Jimp from 'jimp';
+import Jimp from 'jimp';
 import axios from 'axios';
 
 export default async function resizePodcastImage(imageUrl: string, size: number): Promise<Buffer | null> {
@@ -10,34 +10,33 @@ export default async function resizePodcastImage(imageUrl: string, size: number)
         method: 'GET',
         url: imageUrl,
         responseType: 'arraybuffer',
-      }).then((response) => {
-        console.log('@@@ skipping>', !!response);
-        //  return resolve(null);
-        //   console.log(`> resizing image: ${imageUrl}>A1>imageBuffer>`, response.statusText);
-        //   const imageBuffer: ArrayBuffer = response.data;
-        //   return Jimp.read(Buffer.from(imageBuffer));
-        // })
-        // .then((image) => {
-        //   console.log(`> resizing image: ${imageUrl}>B`);
-        //   return image.cover(size, size);
-        // })
-        // .then((image) => {
-        //   console.log(`> resizing image: ${imageUrl}>C`);
-        //   return image.quality(90);
-        // })
-        // .then((image) => {
-        //   console.log(`> resizing image: ${imageUrl}>D`);
-        //   return image.getBufferAsync(Jimp.MIME_PNG);
-        // })
-        // .then((buffer) => {
-        //   console.log(`> resizing image: ${imageUrl}>buffer>`);
-        //   return resolve(buffer);
-        // })
-        // .catch((err) => {
-        //   console.log(`> resizing image: ${imageUrl}>X`);
-        //   console.error(`> Error resizing image: ${imageUrl}:`, err);
-        return resolve(null);
-      });
+      })
+        .then((response) => {
+          console.log(`> resizing image: ${imageUrl}>A1>imageBuffer>`, response.statusText);
+          const imageBuffer: ArrayBuffer = response.data;
+          return Jimp.read(Buffer.from(imageBuffer));
+        })
+        .then((image) => {
+          console.log(`> resizing image: ${imageUrl}>B`);
+          return image.cover(size, size);
+        })
+        .then((image) => {
+          console.log(`> resizing image: ${imageUrl}>C`);
+          return image.quality(90);
+        })
+        .then((image) => {
+          console.log(`> resizing image: ${imageUrl}>D`);
+          return image.getBufferAsync(Jimp.MIME_PNG);
+        })
+        .then((buffer) => {
+          console.log(`> resizing image: ${imageUrl}>buffer>`);
+          return resolve(buffer);
+        })
+        .catch((err) => {
+          console.log(`> resizing image: ${imageUrl}>X`);
+          console.error(`> Error resizing image: ${imageUrl}:`, err);
+          return resolve(null);
+        });
     } catch (err) {
       console.error('resizePodcastImage>', imageUrl, err);
       return resolve(null);
